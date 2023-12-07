0789d3869a717def015bfe4d2f7138f9f0012bc5
Tim Reynolds <timmy.reynolds1121@gmail.com>
2023-12-07 09:33:33 -0500

    NationalNews Page

--- client/src/pages/LocalNews.jsx
+++ client/src/pages/LocalNews.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 
-// As a user interested in local news:
-// As a user, I want to access a dedicated section for local news, allowing me to stay updated on events and developments within my vicinity.
-// As a user, I want my location to be saved so the local news is specific to me. 
-// Acceptance criteria:
-// The app should display news articles specifically related to the user's location or chosen local area.
-// Users should be able to easily navigate to this section and browse local news effortlessly.
-
 const LocalNews = () => {
     return (
         <div>
-          <h3>Local news content here...</h3>
-          {/* local news content here */}
+          <h3>World news content here...</h3>
+          {/* Add Local news content here */}
         </div>
       );
 }
--- client/src/pages/NationalNews.jsx
+++ client/src/pages/NationalNews.jsx
@@ -1,13 +1,77 @@
-import React from 'react';
-// As a user, I want to access a dedicated section for national current events. 
+import React, { useState, useEffect } from "react";
 
 const NationalNews = () => {
-    return (
-        <div>
-          <h3>National news content here...</h3>
-          {/* Add national news content here */}
-        </div>
-      );
-}
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [userCountryCode, setUserCountryCode] = useState(""); 
+
+  const fetchArticles = async (countryCode) => {
+    const apiKey = "8cc2063285f3470b96ff200384478e9b";
+
+    const apiUrl = `https://newsapi.org/v2/top-headlines?apiKey=${apiKey}&country=${countryCode}&pageSize=5`;
+
+    setLoading(true);
+
+    try {
+      const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setArticles(data.articles || []);
+    } catch (error) {
+      console.error("Error fetching articles:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleCountryCodeChange = (event) => {
+    setUserCountryCode(event.target.value);
+  };
+
+  const handleFetchNews = () => {
+    if (userCountryCode.trim() !== "") {
+      fetchArticles(userCountryCode);
+    } else {
+      console.error("Invalid country code");
+    }
+  };
+
+  return (
+    <div>
+      <h3>Local News:</h3>
+      <label>
+        Enter Country Code:{" "}
+        <input
+          type="text"
+          value={userCountryCode}
+          onChange={handleCountryCodeChange}
+        />
+      </label>
+      <button onClick={handleFetchNews}>Fetch News</button>
+
+      {loading ? (
+        <p>Loading articles...</p>
+      ) : (
+        <ul>
+          {articles.map((article) => (
+            <li key={article.title}>
+              <h4>{article.title}</h4>
+              <p>{article.description}</p>
+              <p>Author: {article.author}</p>
+              <p>Published at: {article.publishedAt}</p>
+              <p>
+                Source: {article.source.name}
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default NationalNews;
