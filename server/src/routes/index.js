d136bc41baf2530bd00bc0f95b7597ba118de850
Dylan Holtlund <dkgould39@gmail.com>
2023-12-07 20:53:52 -0600

    added the srtucture for the backend routing for the pages. Need to add the functionality later.

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,4 +5,43 @@ const router = Router();
 
 router.route("/").get(healthCheck);
 
+
+router.route("/local-news").get(
+
+);
+
+
+router.route("/national-news").get(
+
+);
+
+
+router.route("/world-news").get(
+
+);
+
+
+router.route("/sports").get(
+
+);
+
+
+router.route("/weather").get(
+
+);
+
+
+router.route("/user-profile").get(
+
+)
+.post(
+
+)
+.put(
+
+)
+.delete(
+
+);
+
 export default router;
