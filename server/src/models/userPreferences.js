d558a55b38ef1111c423a76f0b0517b38deb803a
Dylan Holtlund <dkgould39@gmail.com>
2023-12-09 17:28:19 -0600

    preferences schema complete

--- server/src/models/userPreferences.js
+++ server/src/models/userPreferences.js
@@ -5,10 +5,11 @@ import { Schema, model } from 'mongoose';
 const preferencesSchema = new Schema(
   {
     countryCode: {
-      // I need help figuring out how to implement the iso-2 coding (this is what will give the 2 letter country format.)
       type: String,
       // I am going to default to us as anyone looking at this will most likely be in the us.
       default: 'US',
+      minLength: 2,
+      maxLength: 2,
       require: true,
     },
     team: {
