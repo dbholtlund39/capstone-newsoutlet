fe5e3231d6d6b41820dd41647d952d5d5870512f
Dylan Holtlund <dkgould39@gmail.com>
2023-12-13 00:47:09 -0600

    added an auth file to set the signin and signup requests

---
+++ client/hooks/Auth.jsx
@@ -0,0 +1,6 @@
+/* This will have the post requests for signup and sign in. I am looking to old work to get a reminder of how to set this up. From what I am seeing
+I will need another hook to pass this into for the useEffect navigation. I was originally going to set this up in a components file. */
+
+import { Reducer, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
