b5d4301bb5073bcdbeefbc6071aa0b4e3ecd260e
Tim Reynolds <timmy.reynolds1121@gmail.com>
2024-01-04 13:50:59 -0500

    weather.css changes

--- client/src/pages/Weather/Weather.jsx
+++ client/src/pages/Weather/Weather.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import "client/src/pages/Weather/Weather.css"
+import "./Weather.css"
 // import axios from "axios"
 import axios from "axios"
 import { WEATHER_API_KEY } from "../../configs/constants"
