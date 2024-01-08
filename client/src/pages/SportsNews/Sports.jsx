0b722bf39448befff1fd4da3081ad88a7250d79a
Amy Belcher <amylei7@hotmail.com>
2024-01-08 12:10:40 -0500

    sports css changes

--- client/src/pages/SportsNews/Sports.jsx
+++ client/src/pages/SportsNews/Sports.jsx
@@ -1,51 +1,58 @@
-import React, { Component } from "react";
-import LatestSportsNews from "./LatestSportsNews";
-import Basketball from "./Nba.jsx";
-import FootballArticles from "./Nfl.news.jsx";
-import MlbNews from "./Mlb.jsx";
-import OtherSports from "./OtherSports.jsx";
+import React, { Component } from "react"
+import LatestSportsNews from "./LatestSportsNews"
+import Basketball from "./Nba.jsx"
+import FootballArticles from "./Nfl.news.jsx"
+import MlbNews from "./Mlb.jsx"
+import OtherSports from "./OtherSports.jsx"
 
 class Sports extends Component {
   render() {
     const contentStyle = {
-      marginTop: "50px",
+      marginTop: "100px",
       marginBottom: "0px",
       padding: "6px",
       borderRadius: "8px",
-      
-    };
+    }
 
     const navStyle = {
-      position: 'sticky',
-      top: '100px',
-      backgroundColor: 'transparent',
-      zIndex: '1000',
-      padding: '50px 0',
-marginBottom: "-160px",
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '100%',
-        };
+      position: "static",
+      top: "100px",
+      backgroundColor: "transparent",
+      zIndex: "1000",
+      padding: "0px",
+      marginBottom: "-160px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+    }
 
     const navLinkStyle = {
       textDecoration: "none",
       color: "blue",
       cursor: "pointer",
-      padding: "20px 15px",
-      margin: "0 5px",
-    
-    };
-
+      padding: "2% 6%",
+      margin: "5px 10px",
+    }
 
     return (
       <div>
         <nav style={navStyle}>
-          <a style={navLinkStyle} href="#home">Home</a>
-          <a style={navLinkStyle} href="#basketball">Basketball</a>
-          <a style={navLinkStyle} href="#footballArticles">Football</a>
-          <a style={navLinkStyle} href="#mlbNews">Baseball</a>
-          <a style={navLinkStyle} href="#other">Other Sports</a>
+          <a style={navLinkStyle} href="#home">
+            Home
+          </a>
+          <a style={navLinkStyle} href="#basketball">
+            Basketball
+          </a>
+          <a style={navLinkStyle} href="#footballArticles">
+            Football
+          </a>
+          <a style={navLinkStyle} href="#mlbNews">
+            Baseball
+          </a>
+          <a style={navLinkStyle} href="#other">
+            Other Sports
+          </a>
         </nav>
 
         <div id="home" style={contentStyle}>
@@ -64,8 +71,8 @@ marginBottom: "-160px",
           <OtherSports />
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default Sports;
+export default Sports
