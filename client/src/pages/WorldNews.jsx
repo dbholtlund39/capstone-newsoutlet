ecad36af9cb9479ab3503734e21b27fb58b62146
Tim Reynolds <timmy.reynolds1121@gmail.com>
2024-01-08 22:03:45 -0500

    Testing for production

--- client/src/pages/WorldNews.jsx
+++ client/src/pages/WorldNews.jsx
@@ -33,7 +33,7 @@ const WorldNews = () => {
             const cachedArticles = JSON.parse(storedData);
             articlesArray.push(...cachedArticles);
           } else {
-            const response = await fetch(`http://3.15.225.91:8080/api/local-news/${countryCode}`);
+            const response = await fetch(`http://localhost:8080/api/local-news/${countryCode}`);
   
             if (!response.ok) {
               throw new Error(`HTTP error! Status: ${response.status}`);
